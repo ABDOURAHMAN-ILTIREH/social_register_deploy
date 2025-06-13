566741cf85f4fa96c38571c8e0db5c01d5b4202c
ABOURAHMAN ILTIREH <111226361+ABDOURAHMAN-ILTIREH@users.noreply.github.com>
2025-06-13 14:32:50 +0300

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,19 @@
 // config/database.js
-
 module.exports = {
+ development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false, // set to false for self-signed certs
+      },
+    },
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
